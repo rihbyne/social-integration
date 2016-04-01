5e8c26aa6ad7185f53a50e4b2e28ff1e3545796c
sudeep_makwana <sudeepmakwana@movingtrumpet.com>
2016-04-01 16:29:29 +0530

    Revert "data insert if collection not there is DB"
    This reverts commit 54b45e941edf724fd4ca13fcbe9813300e34bd73.

--- config/mongo.js
+++ config/mongo.js
@@ -1,50 +1,17 @@
 // Packages
-var mongoose = require('mongoose'); // For Mongoose 
+var mongoose        = require('mongoose');      // For Mongoose 
 
 // Build the connection string 
-var dbURI = 'mongodb://localhost/social_network';
+var dbURI = 'mongodb://localhost/social_network'; 
 
 // Create the database connection 
-mongoose.connect(dbURI);
+mongoose.connect(dbURI); 
 
-var db_server = process.env.DB_ENV || 'primary';
+var db_server  = process.env.DB_ENV || 'primary';
 
 // CONNECTION EVENTS
 // When successfully connected
-mongoose.connection.on('connected', function() {
-    console.log('Mongoose default connection open to ' + dbURI);
-    console.log("Connected to " + db_server + " DB!");
-});
-
-var conn = mongoose.connection;
-// mongoose.connection.db.listCollections({name: 'test_coll'})
-//     .next(function(err, collinfo) {
-//         if (collinfo) {
-//             // The collection exists
-//             console.log(collinfo +'collection exists');
-//         }
-//     });
-
-// [ { name: 'post_mentions', options: {} },
-//  { name: 'users', options: {} },
-//  { name: 'post_hashtags', options: {} },
-//  { name: 'posts', options: {} } ]
-
-conn.on('open', function() {
-    conn.db.listCollections().toArray(function(err, names) {
-        var jsonval = names;
-        var hasMatch = false;
-        for (var index = 0; index < jsonval.length; ++index) {
-            var COL = jsonval[index];
-            if (COL.name == "posts") {
-                hasMatch = true;
-                console.log('posts collectionss exists');
-                break;
-            }
-                console.log('posts collectionss dosent  exists');
-        }
-
-        conn.close();
-
-    });
-});
+mongoose.connection.on('connected', function () {  
+  console.log('Mongoose default connection open to ' + dbURI);
+  console.log("Connected to " + db_server + " DB!");
+}); 

4b3f890740e6d47a42dfb7f9cbc3d416bf920cc5
sudeep_makwana <sudeepmakwana@movingtrumpet.com>
2016-04-20 12:01:03 +0530

    route change - @post.js

--- public/js/custome-script.js
+++ public/js/custome-script.js
@@ -23,4 +23,28 @@
        // show count is RT more then 0
        $( ".btn-retweet span:contains(0)" ).css( "display", "none" );
        $( ".btn-like span:contains(0)" ).css( "display", "none" );
+       
+//        // $( ".btn-like span:contains(0)" ).css( "display", "none" ).removeClass("btn-like-active");
+// // $( "div:contains('John')" ).css( "text-decoration", "underline" );
+// //       if ($('.btn-like span:contains("0")')) {
+// //       //    if ($(this).parent().is('.btn-like')) {
+// //          alert('bluvalue is 0 ');
+
+// //       //    return $(this).parent().addClass('btn-like-active');
+// //      }
+// //      alert('not a 0')
+
+// $('.btn-like').removeClass("btn-like-active")   
+
+// if ($('.btn-like span:contains("0")')) 
+//  { 
+
+//     $('.btn-like').addClass("btn-like-active")   
+//      // $(this).parent().addClass("btn-like-active");
+//  }
+// $('.btn-like').removeClass("btn-like-active")   
+
+     // $(this).parent().removeClass("btn-like");
+
+
   })

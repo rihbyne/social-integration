30a31d4f9e7612fd90cf6a5cc0b1350d48555351
rihbyne <rihen234@gmail.com>
2016-04-25 19:30:35 +0530

    global level config added to st-social-network

--- app.js
+++ app.js
@@ -7,15 +7,20 @@ var port     			= process.env.PORT || 4000;						// Port
 var passport 			= require('passport');							// Used For Authentication
 var flash    			= require('connect-flash');						// For Flash Messages
 var expressValidator 	= require('express-validator');					// For validation
-var server              = require('http').Server(app);
 var bodyParser          = require('body-parser');						// To Accept Post Request
-var path                = require('path');  							
+var path                = require('path');
 var morgan       		= require('morgan');							// Request To The Console
+var bformat = require('bunyan-format');
 var cookieParser 		= require('cookie-parser');						// Read cookies (needed for auth)
 var session      		= require('express-session');
 
+var log = require('./config/logging')()
+
+require('dotenv').config() //loads project specific process.env settings from .env
+require('./app/models/db') //initialize db collections and its data
+
 // Pages
-var mongo               = require('./config/mongo.js');
+var server              = require('http').Server(app);
 var notificationschema  = require('./model/notification_model.js');
 var notification        = require('./api/notification.js');  
 var mailer              = require('./api/mail.js');                     // Mail Functionality

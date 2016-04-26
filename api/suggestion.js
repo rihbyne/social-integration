aa03c42b73f65fde768d3255dc0d38c077ae3769
swanand_searchtrade <swanand.searchtrade@gmail.com>
2016-04-26 16:00:10 +0530

    getsuggestion API Done

--- api/suggestion.js
+++ api/suggestion.js
@@ -54,7 +54,7 @@ var follower 	= require('../app/models/model_followers.js'),
 var getSuggestion = function(req, res){
 
 	var user_id = req.params.user_id;
-	var my_list = [];
+	var my_list = new Array;
 	var i = 0;
 	var loop = 0;
 	
@@ -69,76 +69,76 @@ var getSuggestion = function(req, res){
 
 	follower
 	.find({$and:[{user_id : user_id},{follow_status:true}]},{_id:0})
-	.populate('following_id')
+	.select('following_id')
 	.sort({"follower_since":-1})
+	.limit(1)
 	.exec(function(err, latestFollowed){
 				
 		if(err)
 			res.send(err);
-			
-		res.send(latestFollowed)
-		//console.log('latest followed:', latestFollowed[0]);
 		
-		// follower
-		// .find({$and:[{user_id : latestFollowed[0].following_id},{follow_status:true}]},{_id:0})
-		// .populate('following_id')
-		// .sort({"follower_since":-1})
-		// .lean()
-		// .exec(function(err, followingIds){
+		follower
+		.find({$and:[{user_id : latestFollowed[0].following_id},{follow_status:true}]},{_id:0})
+		.populate('following_id')
+		.limit(6)
+		.lean()
+		.exec(function(err, followingIds){
 			
-			// if(err)
-				// res.send(err);	
+			if(err)
+				res.send(err);	
 				
-			// var arrayLength = followingIds.length;
-			// console.log(followingIds);
-						
-			// async.forEach(followingIds, function (item, cb) {
-			
-				// var followerId = item.following_id;
-				
-				// console.log(followerId);
-			
-				// if(followerId==user_id)
-				// {
-					// loop++;
-					// cb()
-				// }
-			
-				// else
-				// {
-					// follower
-					// .find({$and:[{user_id : user_id},{following_id:followerId},{follow_status:true}]})
-					// .populate('following_id')
-					// .exec(function(err, check){
-						
-						// if(err)
-						// res.send(err);
-						
-						// loop++;
-						// console.log(loop);
+			var arrayLength = followingIds.length;
 
-						// if(!check)
-						// {
-							// my_list[i] = ;
-							// i++
-						// }
-						
-						// if(loop == arrayLength)
-						// {
-							// console.log(my_list);
-						// }
-						
-						// else
-						// {
-							// cb()
-						// }
-					
-					// })
-				// }
+			async.forEach(followingIds, function (item, cb) {
 			
-			// })
+				var followerId = item.following_id._id;
+				var followerUsername = item.following_id.username;
+				var firstName = item.following_id.first_name;
+				var lastName = item.following_id.last_name;
+					
+				if(followerId==user_id)
+				{
+					loop++;
+					cb()
+				}
+			
+				else
+				{
+					follower
+					.count({$and:[{user_id : user_id},{following_id:followerId},{follow_status:true}]})
+					.lean()
+					.exec(function(err, check){
+						
+						if(err)
+							res.send(err);
+						
+						loop++;
+
+						if(!check)
+						{
+							my_list[i] = {
+											id:followerId, 
+											username:followerUsername,
+											firstname : firstName,
+											lastname:lastName
+										};
+							i++
+						}
+						
+						if(loop == arrayLength)
+						{
+							res.send(my_list);
+							return;
+						}
+						
+						cb()	
+							
+					})
+				}
+			
+			})
 		
-		// })
+		})
 	})
 }	
 	

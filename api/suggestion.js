c7ac423258f7772a158ba13adc5a6250546dca7e
swanand_searchtrade <swanand.searchtrade@gmail.com>
2016-04-26 14:43:13 +0530

    add random suggestion code in suggestion.js

--- api/suggestion.js
+++ api/suggestion.js
@@ -54,6 +54,9 @@ var follower 	= require('../app/models/model_followers.js'),
 var getSuggestion = function(req, res){
 
 	var user_id = req.params.user_id;
+	var my_list = [];
+	var i = 0;
+	var loop = 0;
 	
     req.checkParams('user_id', 'User Id is mandatory').notEmpty();
 
@@ -66,26 +69,76 @@ var getSuggestion = function(req, res){
 
 	follower
 	.find({$and:[{user_id : user_id},{follow_status:true}]},{_id:0})
-	.select('following_id')
+	.populate('following_id')
 	.sort({"follower_since":-1})
-	.limit(1)
 	.exec(function(err, latestFollowed){
 				
 		if(err)
 			res.send(err);
-		
-		follower
-		.find({$and:[{user_id : latestFollowed[0].following_id},{follow_status:true}]},{_id:0})
-		.select('following_id')
-		.sort({"follower_since":-1})
-		.exec(function(err, followingIds){
 			
-			if(err)
-				res.send(err);	
-						
-			console.log(followingIds);
+		res.send(latestFollowed)
+		//console.log('latest followed:', latestFollowed[0]);
 		
-		})
+		// follower
+		// .find({$and:[{user_id : latestFollowed[0].following_id},{follow_status:true}]},{_id:0})
+		// .populate('following_id')
+		// .sort({"follower_since":-1})
+		// .lean()
+		// .exec(function(err, followingIds){
+			
+			// if(err)
+				// res.send(err);	
+				
+			// var arrayLength = followingIds.length;
+			// console.log(followingIds);
+						
+			// async.forEach(followingIds, function (item, cb) {
+			
+				// var followerId = item.following_id;
+				
+				// console.log(followerId);
+			
+				// if(followerId==user_id)
+				// {
+					// loop++;
+					// cb()
+				// }
+			
+				// else
+				// {
+					// follower
+					// .find({$and:[{user_id : user_id},{following_id:followerId},{follow_status:true}]})
+					// .populate('following_id')
+					// .exec(function(err, check){
+						
+						// if(err)
+						// res.send(err);
+						
+						// loop++;
+						// console.log(loop);
+
+						// if(!check)
+						// {
+							// my_list[i] = ;
+							// i++
+						// }
+						
+						// if(loop == arrayLength)
+						// {
+							// console.log(my_list);
+						// }
+						
+						// else
+						// {
+							// cb()
+						// }
+					
+					// })
+				// }
+			
+			// })
+		
+		// })
 	})
 }	
 	

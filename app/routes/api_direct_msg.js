cc0b09f908d3662cda2b9cde566f5a6e85b08539
rihbyne <rihen234@gmail.com>
2016-04-27 19:42:13 +0530

    refactored api endpoints

--- app/routes/api_direct_msg.js
+++ app/routes/api_direct_msg.js
@@ -8,6 +8,6 @@ router.get('/:user_id/msgtexts/:id', ctrlDirectMsg.getDMById)
 router.post('/:user_id/msgtexts', ctrlDirectMsg.startDM)
 router.put('/:user_id/msgtexts/:id', ctrlDirectMsg.resumeDMById)
 router.delete('/:user_id/msgtexts/:id/msg/:msg_id', ctrlDirectMsg.removeDMByMsgId)
-router.patch('/:user_id/msgtexts/:id/msg/:msg_id', ctrlDirectMsg.flagMsgId)
+router.post('/:user_id/msgtexts/:id/msg/:msg_id', ctrlDirectMsg.flagMsgId)
 
 module.exports = router;

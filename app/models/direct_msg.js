ea3d5a3f38df1fc8d55d043b4fc5cdc927057c88
rihbyne <rihen234@gmail.com>
2016-04-27 19:44:14 +0530

    refactor collections plus added pre hook middleware to catch duplicate key index error at runtime

--- app/models/direct_msg.js
+++ app/models/direct_msg.js
@@ -3,6 +3,9 @@ var mongoose = require('mongoose')
 var User = mongoose.model('User')
 var Flag = mongoose.model('Flag')
 
+var util = require('util')
+var log = require('../../config/logging')()
+
 var oneToOneMsgSessionSchema = new mongoose.Schema({
   user_one_fk_key: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   user_two_fk_key: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
@@ -10,8 +13,29 @@ var oneToOneMsgSessionSchema = new mongoose.Schema({
   session_time: {type: Date, default: Date.now}
 });
 
+oneToOneMsgSessionSchema.pre('save', true, function(next, done) {
+  var self = this
+  OneToOneMsgSession
+                    .find({
+                      $and: [{user_one_fk_key: this.user_one_fk_key}, {user_two_fk_key: this.user_two_fk_key}]
+                    }, function(err, data) {
+                      if (err) {
+                        log.error(err)
+                        next(err)
+                      } else if (data.length) {
+                        log.warn(util.inspect(data))
+                        self.invalidate("_id", "session already exist")
+                        next(new Error("session already exist"))
+                      } else {
+                        log.info("session save pre hook passed")
+                        next()
+                      }
+                    })
+  done()
+})
+
 var oneToOneMsgTextSchema = new mongoose.Schema({
-  msgText: {type: String, required: true},
+  msg_text: {type: String, required: true},
   user_id_fk_key: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   ip: {type: String, required:true},
   msg_time: {type: Date, default: Date.now},
@@ -23,5 +47,5 @@ var flagMsgSchema = new mongoose.Schema({
   flag_action: {type: mongoose.Schema.Types.Number , ref: 'Flag'}
 })
 
-mongoose.model('OneToOneMsgSession', oneToOneMsgSessionSchema, 'OneToOneMsgSessions')
+var OneToOneMsgSession = mongoose.model('OneToOneMsgSession', oneToOneMsgSessionSchema, 'OneToOneMsgSessions')
 mongoose.model('OneToOneMsgText', oneToOneMsgTextSchema, 'OneToOneMsgTexts')

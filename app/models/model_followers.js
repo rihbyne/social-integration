89f2800051480489bd83d2a63817df8c2e9615df
swanand_searchtrade <swanand.searchtrade@gmail.com>
2016-04-29 12:18:32 +0530

    modified Schema

--- app/models/model_followers.js
+++ app/models/model_followers.js
@@ -1,57 +1,16 @@
-var mongoose = require('mongoose');
-var User = require('./user.js');
+var mongoose 		= require('mongoose');				// For Mongoose
+var User 			= require('./user.js');				// For Referring User Collection
 
 var userfollowersSchema = mongoose.Schema({
-    user_id: {  //logged in user id
-        type: String,
-        ref: "User",
-        required: [true, 'Why no user_id?']
-    },
-    following_id: { //following/follower id
-        type: String,
-		ref: 'User',
-        required: [true, 'Why no following_id?']
-    },
-    follower_since: { //following from
-        type: Date,
-        default: Date.now
-    },
-	// recent_activity: {
-		// type : Date,
-		// defalult : Date.now
-	// },
-	// recent_reTweet_activity: {
-		// type : Date,
-		// defalult : Date.now
-	// },
-	// recent_reply_activity: {
-		// type : Date,
-		// defalult : Date.now
-	// },
-    block: { //for block user post
-        type: Boolean,
-        date: {
-            type: Date,
-            default: Date.now
-        }
-    },
-    mute: { //for notification mute
-        type: Boolean,
-        date: {
-            type: Date,
-            default: Date.now
-        }
-    },
-    follow_back: { // follow back status
-        type: Boolean,
-        default: false //by default user will not follow other user
-    },
-    follow_status: { //unlink follow status
-        type: Boolean,
-        default: true // by defalult user will link with other user
-    },
-}, {
-    versionKey: false
-});
+    
+	user_id: 		{ type: String, ref: "User", required: [true, 'Why no user_id?']},			// User Id (User Id From user Colloction) [Who is Following] 
+    following_id: 	{ type: String, ref: 'User', required: [true, 'Why no following_id?']},		// User Id (User Id From user Colloction) [To Whom user_id is Following] 
+    follower_since: { type: Date, default: Date.now },											// Date From user_id Started following following_id
+    block: 			{ type: Boolean, date: { type: Date, default: Date.now }},					// Status That Decides user_id blocked following_id or not
+    mute: 			{ type: Boolean, date: { type: Date, default: Date.now }},					// Status That Decides user_id Muted following_id or not
+    follow_back: 	{ type: Boolean, default: false },											// true if following_id is also following user_id
+    follow_status: 	{ type: Boolean, default: true },											// Status That decides user_id is currently following following_id or not
+	
+},{ versionKey: false });
 
 module.exports = mongoose.model('user_followers', userfollowersSchema);

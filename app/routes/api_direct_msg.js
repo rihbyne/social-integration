ac972407b305b1bdf3e9123866d10990776e34e5
rihbyne <rihen234@gmail.com>
2016-04-30 16:10:46 +0530

    added GET flagoptions endpoint and updated POST method for flagging msgID

--- app/routes/api_direct_msg.js
+++ app/routes/api_direct_msg.js
@@ -8,6 +8,7 @@ router.get('/:user_id/msgtexts/:id', ctrlDirectMsg.getDMById)
 router.post('/:user_id/msgtexts', ctrlDirectMsg.startDM)
 router.put('/:user_id/msgtexts/:id', ctrlDirectMsg.resumeDMById)
 router.delete('/:user_id/msgtexts/:id/msg/:msg_id', ctrlDirectMsg.removeDMByMsgId)
-router.post('/:user_id/msgtexts/:id/msg/:msg_id', ctrlDirectMsg.flagMsgId)
+router.get('/flagoptions', ctrlDirectMsg.getFlagOptions)
+router.post('/:user_id/msgtexts/:id/msg/:msg_id/flag', ctrlDirectMsg.flagMsgId)
 
 module.exports = router;
